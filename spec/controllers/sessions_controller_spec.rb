54fbda61f963b2a5d457a0c3c4c764f48fd37971
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-20 19:42:04 -0700

    Create Session controller and associated resources

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  describe 'GET #new' do
+    it "renders the new page" do
+      
+    end
+  end
+
+  describe 'PUT #create' do
+
+  end
+
+  describe 'DELETE #destroy' do
+
+  end
+end
