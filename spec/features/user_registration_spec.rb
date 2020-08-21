4bd7848d078b4914d67e020234cac4b9853d1349
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-21 16:34:43 -0700

    Add pending tests for user registration feature
    At this time, user_registration_spec does not include tests for ways
    in which an invalid registration attempt could be made. This commit
    includes the 8 such ways as pending tests to be implemented at a later
    date.

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "UserRegistrations", type: :feature do
     expect(page).to have_content 'Register'
   end
 
-  feature 'registering a new user' do
+  feature 'registering a new user with valid inputs' do
     before(:each) do
       visit new_user_url
       fill_in 'username', with: 'jason'
@@ -28,5 +28,16 @@ RSpec.feature "UserRegistrations", type: :feature do
     end
 
     scenario 'user is displayed as logged in'
+  end
+
+  feature 'registering a user with invalid inputs' do
+    scenario 'empty username and empty password'
+    scenario 'empty username and invalid (short) password'
+    scenario 'empty username and valid password'
+    scenario 'duplicate username and empty password'
+    scenario 'duplicate username and invalid (short) password'
+    scenario 'duplicate username and valid password'
+    scenario 'valid username and empty password'
+    scenario 'valid username and invalid (short) password'
   end
 end
