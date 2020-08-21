27df7c86d9c170e902727147d36ebc08bb1f04e8
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-21 14:17:12 -0700

    Add csrf auth token input to new user form ...
    ... Update users controller #create to automatically log in (create new session) a registered user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login!(@user)
       render :show
     else
       flash[:errors] = @user.errors.full_messages
