62c78dc32afdb4485d74926c5b4d122bfe79a707
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-21 16:22:08 -0700

    Create ApplicationController#logout! and associated tests
    This commit adds logout functionality to the auth system; logout!
    is called in SessionsController#destroy, and does the following:
    
    1. sets current_user to nil
    2. resets the session_token for the logged-out user
    3. sets session[:session_token] to nil

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe ApplicationController, type: :controller do
       expect(subject.current_user).to eq(@user)
     end
   end
+
+  describe '#logout!' do
+    before(:each) do
+      User.create(username: 'jason', password: 'good_password')
+      @user = User.find_by(username: 'jason')
+      subject.login!(@user)
+      @initial_session_token = @user.session_token
+      subject.logout!
+    end
+
+    after(:each) do
+      @user.destroy
+    end
+    it "sets session[:session_token] to nil" do
+      expect(session[:session_token]).to be_nil
+    end
+
+    it "sets current_user to nil" do
+      expect(subject.current_user).to be_nil
+    end
+
+    it "resets the session_token of the logged-out user" do
+      expect(@user.session_token).to_not eq(@initial_session_token)
+    end
+  end
 end
