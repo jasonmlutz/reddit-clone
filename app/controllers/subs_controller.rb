29b49385f382697a1a4facdb9484a57f4f9dc481
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-29 16:23:29 -0700

    Build SubsController#create

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,14 +1,22 @@
 class SubsController < ApplicationController
+  skip_before_action :require_login, only: [:index]
   def index
-
+    render :index
   end
 
   def create
-
+    @sub = Sub.new(sub_params)
+    @sub.user_id = current_user.id
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:alert] = 'Invalid submission. Title and description are required.'
+      redirect_to new_sub_url
+    end
   end
 
   def new
-
+    render :new
   end
 
   def edit
@@ -25,5 +33,11 @@ class SubsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def sub_params
+    params.require(:sub).permit(:title, :description)
   end
 end
